d56238bcef990570e306d6a63f123cfc3067bacd
Idefinitelynos <95195388+Idefinitelynos@users.noreply.github.com>
2022-10-05 20:03:13 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+Song1=""
+song2=""
+function preload() {
+    song2=loadSound("music2.mp3")
+    song1=loadSound("music.mp3")
+}
+function setup() {
+    canvas=createCanvas(600, 500)
+    canvas.center()
+    video=createCapture(VIDEO)
+    video.hide()
+}
+function draw() {
+    image(video, 0, 0, 600, 500)
+}
